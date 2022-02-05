580cef49b6d328435da1f47bc0ffe60a8703a287
Pummy Jha <64890582+Pummy12@users.noreply.github.com>
2022-02-06 00:15:25 +0530

    to check a dictionary is empty or not

---
+++ Q19.py
@@ -0,0 +1,9 @@
+#Q19.Write a Python program to check a dictionary is empty or not.
+empty_dict = {}
+
+if empty_dict:
+  print("Dict not empty")
+else:
+  print("dict emptyy")
+
+# Returns: Dictionary is empty!
