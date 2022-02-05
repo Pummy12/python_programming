bfb4b8807de1e82575617077d27698370b67e2b9
Pummy Jha <64890582+Pummy12@users.noreply.github.com>
2022-02-06 00:07:43 +0530

    to sort the elements of list in descending order

---
+++ Q5.py
@@ -0,0 +1,5 @@
+#Q5.Write a python program to sort the elements of the given list in descending order.
+nums = [1, 5, 3, 4, 2, 10, 6, 8, 7, 9]
+nums.sort()
+nums.sort(reverse=True)
+print('List in Descending Order: ', nums)
