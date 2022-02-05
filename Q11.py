c6110e7f03baf790190a29e482de178b17a63e39
Pummy Jha <64890582+Pummy12@users.noreply.github.com>
2022-02-06 00:11:06 +0530

    to find the largest number from a set

---
+++ Q11.py
@@ -0,0 +1,5 @@
+#Q11. Write a python program to find the largest number from a set.
+my_set={1,2,3,6,8,9,4,5,61,34,78,92}
+print("set item:",my_set)
+print("largest item in the set is:")
+print(max(my_set))
