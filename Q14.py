8f73cf17b934ef046ad4e032d21abaf5ab95de1a
Pummy Jha <64890582+Pummy12@users.noreply.github.com>
2022-02-06 00:12:43 +0530

    to remove a key from a dictionary

---
+++ Q14.py
@@ -0,0 +1,7 @@
+#Q14.Write a Python program to remove a key from a dictionary.
+my_dict={1:'abc',2:'def',3:'ghi',4:'jkl',5:'mno',6:'pqr',7:'stu',8:'vwxyz'}
+print(my_dict)
+print("Removing key element:")
+print(my_dict.pop(8))
+print("The dictionary becomes:")
+print(my_dict)
