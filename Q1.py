3aadf47bc74311eb7fed2dffb571da19d0dc7539
Pummy Jha <64890582+Pummy12@users.noreply.github.com>
2022-02-06 00:04:44 +0530

    to find the number of items available in the list

---
+++ Q1.py
@@ -0,0 +1,9 @@
+#Q1 Write a python program to find the number of items available in the list.
+list = [] 
+list.append(1)
+list.append(2)
+list.append(3)
+list.append(4)
+print(list)
+print("No of elements in list are")
+print(len(list))
