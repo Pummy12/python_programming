92e5c98610fdbaae00f6daabd6fcece51b1a8ab9
Pummy Jha <64890582+Pummy12@users.noreply.github.com>
2022-02-06 00:11:37 +0530

    to find sum of all the items in a set

---
+++ Q12.py
@@ -0,0 +1,6 @@
+#Q12. Write a python program to find sum of all the items in a set.
+my_set={1,2,3,4,5,6,7,8,9,10}
+print("set elements:",my_set)
+Sum=sum(my_set)
+print("sum of the elements of the set is:")
+print(Sum)
